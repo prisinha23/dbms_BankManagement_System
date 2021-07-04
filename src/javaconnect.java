714ab0c53deee1f63851751a59553e42d3ce34c6
Priyesh Sinha <51755338+prisinha23@users.noreply.github.com>
2021-07-04 17:25:49 +0530

    Priyesh-2021-5th sem dbms

---
+++ src/javaconnect.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+import java.sql.*;
+
+public class javaconnect {
+	Connection conn = null;
+	
+	public static Connection ConncrDB() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Priyesh\\Desktop\\dbms\\labproject\\Bank.db");
+			return conn;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+
+}
